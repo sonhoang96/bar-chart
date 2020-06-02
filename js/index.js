e2df6c9c6e4317c4ae68ab996ba1f4ac6b55a0f3
sonhoang96 <sonhoangit96@gmail.com>
2020-06-03 01:40:20 +0700

    second commit

--- js/index.js
+++ js/index.js
@@ -1,3 +1,27 @@
-const data = d3.select('body')
-			   .append('h1')
-			   .text('D3 is so great');
+var width = 700;
+var height = 400;
+
+var infoBoard = d3.select('.data-board')
+				  .append('div')
+				  .attr('id','info-board')
+				  .style('opacity', 1);
+
+var hover = d3.select('.data-board')
+				  .append('div')
+				  .attr('class','overlay')
+				  .style('opacity', 1);
+
+var chart = d3.select('.data-board')
+				  .append('div')
+				  .attr('class','chart')
+				  .attr('width', width)
+				  .attr('height', height)
+				  
+const request =  new XMLHttpRequest();
+      request.open('GET', 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json', true);
+      request.send();
+      request.onload = function(){
+    	const json = JSON.parse(request.responseText);
+
+    	console.log(json.data)
+    }
